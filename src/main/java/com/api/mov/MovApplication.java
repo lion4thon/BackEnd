b36ca8ff77eb2702ec70a5f658fd29fccc1dfffb
naeunmin <babaleo115@gmail.com>
2025-10-25 21:41:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/api/mov/MovApplication.java
@@ -0,0 +1,13 @@
+package com.api.mov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/mov/MovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
