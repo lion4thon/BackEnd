6a6c08adfc63d2d71047435a59a51d39f6e57f0c
naeunmin <babaleo115@gmail.com>
2025-10-26 19:18:55 +0900

    feat : JWT 추가 및 로그인 url 추가

--- src/main/java/com/api/mov/global/config/SecurityConfig.java
+++ src/main/java/com/api/mov/global/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.api.mov.global.config;
 
+import com.api.mov.global.jwt.JwtAuthenticationFilter;
+import com.api.mov.global.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,8 +38,13 @@ public class SecurityConfig {
 
                 //HTTP 요청 인가 규칙 설정
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/users").permitAll()
-                );
+                        //로그인, 회원가입 API는 인증 없이 접근 허용
+                        .requestMatchers("/api/users","api/auth/login").permitAll()
+
+                        //나머지 요청은 인증 필요
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
