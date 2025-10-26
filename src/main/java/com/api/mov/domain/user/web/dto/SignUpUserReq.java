cfb4888296a5dbb11e14c11f7a04e2887df8f389
naeunmin <babaleo115@gmail.com>
2025-10-26 18:18:45 +0900

    refactor : 파일 이름 변경

--- src/main/java/com/api/mov/domain/user/web/dto/SignUpUserReq.java
+++ src/main/java/com/api/mov/domain/user/web/dto/SignUpUserReq.java
@@ -3,7 +3,7 @@ package com.api.mov.domain.user.web.dto;
 import lombok.Getter;
 
 @Getter
-public class UserSignUpReq {
+public class SignUpUserReq {
     private String username;
     private String password;
 }
