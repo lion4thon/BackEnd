d080c8d81c46aafe8869e7a57ae7c64d6f30936f
naeunmin <babaleo115@gmail.com>
2025-10-28 16:50:52 +0900

    feat : 유저, 종목 중간 매핑 테이블 작성

---
+++ src/main/java/com/api/mov/domain/pass/entity/UserPass.java
@@ -0,0 +1,28 @@
+package com.api.mov.domain.pass.entity;
+
+import com.api.mov.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pass_id", nullable = false)
+    private Pass pass;
+}
