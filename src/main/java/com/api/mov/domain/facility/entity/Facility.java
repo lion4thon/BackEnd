f2937940698ba3d57046145eb3bec05394f16b82
naeunmin <babaleo115@gmail.com>
2025-10-28 10:47:52 +0900

    feat : 협력 업체 테이블 작성

---
+++ src/main/java/com/api/mov/domain/facility/entity/Facility.java
@@ -0,0 +1,48 @@
+package com.api.mov.domain.facility.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Facility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; //시설명
+
+    @Column(nullable = false)
+    private String contact; //연락처
+
+    @Column(nullable = false)
+    private String address; //시설 주소
+
+    @Column(nullable = false)
+    private Double rating = 0.0; //평균 평점
+
+
+    @OneToMany(mappedBy = "facility", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+
+    //평점 메서드
+    public void updateRating() {
+        if (reviews == null || reviews.isEmpty()){
+            this.rating = 0.0;
+        } else {
+            this.rating = reviews.stream()
+                    .mapToDouble(Review::getRating)
+                    .average()
+                    .orElse(0.0);
+        }
+    }
+}
