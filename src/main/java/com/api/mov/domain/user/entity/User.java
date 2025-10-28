ffdac0b0d323af06796893204825e4a54be962c6
naeunmin <babaleo115@gmail.com>
2025-10-28 16:44:39 +0900

    feat : 예약, 리뷰 테이블과 매핑

--- src/main/java/com/api/mov/domain/user/entity/User.java
+++ src/main/java/com/api/mov/domain/user/entity/User.java
@@ -1,8 +1,13 @@
 package com.api.mov.domain.user.entity;
 
+import com.api.mov.domain.facility.entity.Review;
+import com.api.mov.domain.reservation.entity.Reservation;
 import com.api.mov.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Getter
@@ -24,4 +29,10 @@ public class User extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reservation> reservations = new ArrayList<>();
 }
