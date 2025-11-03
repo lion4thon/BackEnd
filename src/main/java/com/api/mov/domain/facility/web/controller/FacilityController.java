0299bd0dbf5b177954af7e4187b38b2d7200ffb7
naeunmin <babaleo115@gmail.com>
2025-11-03 16:03:47 +0900

    feat : [패키지 생성] 협력 업체 조회 엔드포인트 작성

---
+++ src/main/java/com/api/mov/domain/facility/web/controller/FacilityController.java
@@ -0,0 +1,35 @@
+package com.api.mov.domain.facility.web.controller;
+
+import com.api.mov.domain.facility.service.FacilityService;
+import com.api.mov.domain.facility.web.dto.FacilityRes;
+import com.api.mov.global.response.SuccessResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class FacilityController {
+
+    private final FacilityService facilityService;
+
+    @GetMapping("/facilities")
+    public ResponseEntity<SuccessResponse<?>> getFacilities(@RequestParam String sportName,
+                                                            @PageableDefault(size = 10, sort = "id") Pageable pageable) {
+
+        Page<FacilityRes> facilityResPage = facilityService.getFacilityList(sportName, pageable);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessResponse.ok(facilityResPage));
+    }
+}
