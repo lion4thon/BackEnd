2f061fd07d8a88ea41bc62e0341963887afa2a17
naeunmin <babaleo115@gmail.com>
2025-11-03 18:43:26 +0900

    feat : 협력업체 상세정보 조회 인터페이스 작성 및 구현

--- src/main/java/com/api/mov/domain/facility/service/FacilityServiceImpl.java
+++ src/main/java/com/api/mov/domain/facility/service/FacilityServiceImpl.java
@@ -2,10 +2,12 @@ package com.api.mov.domain.facility.service;
 
 import com.api.mov.domain.facility.entity.Facility;
 import com.api.mov.domain.facility.repository.FacilityRepository;
+import com.api.mov.domain.facility.web.dto.FacilityDetailRes;
 import com.api.mov.domain.facility.web.dto.FacilityRes;
 import com.api.mov.domain.pass.entity.Sport;
 import com.api.mov.domain.pass.repository.SportRepository;
 import com.api.mov.global.exception.CustomException;
+import com.api.mov.global.response.code.facility.FacilityErrorResponseCode;
 import com.api.mov.global.response.code.sport.SportErrorResponseCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -30,8 +32,29 @@ public class FacilityServiceImpl implements FacilityService {
         return facilityList.map(this::toFacilityRes);
     }
 
+    @Override
+    public FacilityDetailRes getFacilityDetail(Long facilityId) {
+        Facility facility = facilityRepository.findById(facilityId)
+                .orElseThrow(()-> new CustomException(FacilityErrorResponseCode.NOT_FOUND_FACILITY));
+
+        return new FacilityDetailRes(
+                facility.getId(),
+                facility.getName(),
+                facility.getAddress(),
+                facility.getDetailAddress(),
+                facility.getContact(),
+                facility.getPrice(),
+                facility.getFeatures(),
+                facility.getWeekdayHours(),
+                facility.getWeekendHours(),
+                facility.getHolidayClosedInfo(),
+                facility.getAccessGuide()
+        );
+    }
+
     private FacilityRes toFacilityRes(Facility facility) {
         return new FacilityRes(
+                facility.getId(),
                 facility.getName(),
                 facility.getAddress(),
                 facility.getPrice() // Facility 엔티티에 price 필드가 있다고 가정
