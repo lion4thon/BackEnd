ce0989a183f35184691501dda5ffdaf7a216fdbe
naeunmin <babaleo115@gmail.com>
2025-11-03 16:06:18 +0900

    refactor : 협력업체 테이블에 가격 추가 및 운동종목테이블과 연결

--- src/main/java/com/api/mov/domain/facility/entity/Facility.java
+++ src/main/java/com/api/mov/domain/facility/entity/Facility.java
@@ -1,6 +1,7 @@
 package com.api.mov.domain.facility.entity;
 
 
+import com.api.mov.domain.pass.entity.Sport;
 import com.api.mov.domain.reservation.entity.Reservation;
 import com.api.mov.global.entity.BaseEntity;
 import jakarta.persistence.*;
@@ -33,10 +34,17 @@ public class Facility extends BaseEntity {
     private String detailAddress; //상세 주소
 
     @Column(nullable = false)
+    private int price; //업장 이용 가격
+
+    @Column(nullable = false)
     private String postCode; //우편 번호
 
     @Column(nullable = false)
     private Double rating = 0.0; //평균 평점
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sport_id", nullable = false)
+    private Sport sport;
 
 
     @OneToMany(mappedBy = "facility", cascade = CascadeType.ALL, orphanRemoval = true)
