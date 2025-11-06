ce604144b6605232d19ddcd552c57b153eb4e91f
naeunmin <babaleo115@gmail.com>
2025-11-06 09:50:52 +0900

    feat : 예약 쿼리문 작성

---
+++ src/main/java/com/api/mov/domain/reservation/repository/ReservationRepository.java
@@ -0,0 +1,24 @@
+package com.api.mov.domain.reservation.repository;
+
+import com.api.mov.domain.facility.entity.Facility;
+import com.api.mov.domain.reservation.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+
+    //취소되지 않은 예약 중 요청된 시간과 겹치는 예약이 있는지 찾아주는 역할
+    @Query("SELECT r FROM Reservation r " +
+           "WHERE r.facility = :facility " +
+           "AND r.status != com.api.mov.domain.reservation.entity.ReservationStatus.CANCELLED " +
+           "AND r.startTime < :endTime " +
+           "AND r.endTime > :startTime")
+    List<Reservation> findOverlappingReservations(@Param("facility") Facility facility,
+                                                  @Param("startTime") LocalDateTime startTime,
+                                                  @Param("endTime") LocalDateTime endTime);
+}
