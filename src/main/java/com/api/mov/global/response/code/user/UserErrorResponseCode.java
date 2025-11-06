8440e63b9ca0516612b1050aafc65f993374d4aa
naeunmin <babaleo115@gmail.com>
2025-11-06 09:49:49 +0900

    feat : 유저 에러코드 추가

--- src/main/java/com/api/mov/global/response/code/user/UserErrorResponseCode.java
+++ src/main/java/com/api/mov/global/response/code/user/UserErrorResponseCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UserErrorResponseCode implements BaseResponseCode {
-    DUPLICATE_USERNAME_409("DUPLICATE_USERNAME_409", 409, "사용자 이름이 이미 존재합니다.");
+    DUPLICATE_USERNAME_409("DUPLICATE_USERNAME_409", 409, "사용자 이름이 이미 존재합니다."),
+    USER_NOT_FOUND_404("USER_NOT_FOUND_404",404,"해당 유저를 찾을 수 없습니다.");
 
 
 
