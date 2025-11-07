8cac73913cd4102be22a4d9f08aac7da24b4ff34
zzuhannn <lunaaeric41@naver.com>
2025-11-07 14:04:39 +0900

    feat: api 엔드포인트 수정

--- src/main/java/com/api/mov/domain/survey/web/controller/SurveyController.java
+++ src/main/java/com/api/mov/domain/survey/web/controller/SurveyController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/")
+@RequestMapping("/api/survey")
 public class SurveyController {
 
     private final SurveyService surveyService;
