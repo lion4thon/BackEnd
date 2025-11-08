7f38e9053295d73b6d2a4bf7607c28b539e53eac
naeunmin <babaleo115@gmail.com>
2025-11-08 15:17:25 +0900

    refactor : 역직렬화를 위한 Setter 추가

--- src/main/java/com/api/mov/domain/reservation/web/dto/ReservationCreateReq.java
+++ src/main/java/com/api/mov/domain/reservation/web/dto/ReservationCreateReq.java
@@ -2,10 +2,12 @@ package com.api.mov.domain.reservation.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 public class ReservationCreateReq {
 
     private Long facilityId;
--- src/main/java/com/api/mov/domain/user/web/dto/SignUpUserReq.java
+++ src/main/java/com/api/mov/domain/user/web/dto/SignUpUserReq.java
@@ -1,10 +1,12 @@
 package com.api.mov.domain.user.web.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 
 //회원가입
 @Getter
+@Setter
 public class SignUpUserReq {
     private String username;
     private String password;
