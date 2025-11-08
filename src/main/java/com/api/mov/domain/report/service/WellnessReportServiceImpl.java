bd2258e24c6d5fb2b1871f1890216bcffa539714
zzuhannn <lunaaeric41@naver.com>
2025-11-09 01:45:29 +0900

    refactor: userId 파라미터 추가

--- src/main/java/com/api/mov/domain/report/service/WellnessReportServiceImpl.java
+++ src/main/java/com/api/mov/domain/report/service/WellnessReportServiceImpl.java
@@ -1,19 +1,19 @@
 package com.api.mov.domain.report.service;
 
-import com.api.mov.domain.pass.entity.Sport;
-import com.api.mov.domain.pass.repository.SportRepository;
+import com.api.mov.domain.pass.entity.Pass;
+import com.api.mov.domain.pass.repository.PassRepository;
 import com.api.mov.domain.report.entity.WellnessReport;
 import com.api.mov.domain.report.repository.WellnessReportRepository;
 import com.api.mov.domain.report.web.dto.CreateWellnessReportReq;
 import com.api.mov.domain.report.web.dto.CreateWellnessReportRes;
 import com.api.mov.domain.report.web.dto.GenerateReportRes;
+import com.api.mov.domain.report.web.dto.GetWellnessReportRes;
 import com.api.mov.domain.user.entity.User;
 import com.api.mov.domain.user.repository.UserRepository;
 import com.api.mov.global.exception.CustomException;
 import com.api.mov.global.external.gpt.dto.LlmReportDto;
 import com.api.mov.global.external.gpt.service.LlmService;
 import com.api.mov.global.response.code.report.ReportErrorResponseCode;
-import com.api.mov.global.response.code.sport.SportErrorResponseCode;
 import com.api.mov.global.response.code.user.UserErrorResponseCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,28 +25,26 @@ public class WellnessReportServiceImpl implements WellnessReportService {
 
     private final WellnessReportRepository wellnessReportRepository;
     private final UserRepository userRepository;
-    private final SportRepository sportRepository;
     private final LlmService llmService;
-
+    private final PassRepository passrepository;
 
     @Override
     @Transactional
-    public CreateWellnessReportRes createReport(CreateWellnessReportReq createWellnessReportReq) {
-        // 사용자 조회
-        User user = userRepository.findById(createWellnessReportReq.getUserId())
-                .orElseThrow(() -> new CustomException(UserErrorResponseCode.NOT_FOUND_USER_404));
+    public CreateWellnessReportRes createReport(CreateWellnessReportReq createWellnessReportReq,Long userID) {
 
-        // 운동 종목 조회
-        Sport sport = sportRepository.findById(createWellnessReportReq.getSportId())
-                .orElseThrow(() -> new CustomException(SportErrorResponseCode.NOT_FOUND_SPORT));
+        User user = userRepository.findById(userID)
+                .orElseThrow(() -> new CustomException(UserErrorResponseCode.USER_NOT_FOUND_404));
+
+        // pass 종목 조회
+        Pass pass = passrepository.findById(createWellnessReportReq.getPassId())
+                .orElseThrow(() -> new CustomException(ReportErrorResponseCode.NOT_FOUND_PASS));
 
         // TODO : 패키지 완료 여부 검증 로직
-
 
         // 반환
         WellnessReport wellnessReport = WellnessReport.builder()
                 .user(user)
-                .sport(sport)
+                .pass(pass)
                 .workoutIntensity(createWellnessReportReq.getWorkoutIntensity())
                 .postWorkoutCondition(createWellnessReportReq.getPostWorkoutCondition())
                 .muscleActivationAreas(createWellnessReportReq.getMuscleActivationAreas())
@@ -59,7 +57,7 @@ public class WellnessReportServiceImpl implements WellnessReportService {
         return new CreateWellnessReportRes(
                 savedReport.getId(),
                 savedReport.getUser().getId(),
-                savedReport.getSport().getId(),
+                savedReport.getPass().getId(),
                 savedReport.getWorkoutIntensity(),
                 savedReport.getPostWorkoutCondition(),
                 savedReport.getMuscleActivationAreas(),
@@ -70,7 +68,7 @@ public class WellnessReportServiceImpl implements WellnessReportService {
     }
     @Override
     @Transactional
-    public GenerateReportRes generateLlmReport(Long reportId) {
+    public GenerateReportRes generateLlmReport(Long reportId, Long userId) {
 
         WellnessReport report = wellnessReportRepository.findById(reportId)
                 .orElseThrow(() -> new CustomException(ReportErrorResponseCode.NOT_FOUND_REPORT));
@@ -84,7 +82,6 @@ public class WellnessReportServiceImpl implements WellnessReportService {
             );
         }
 
-        // 👇 [수정] LlmService가 Mono를 반환하므로, .block()으로 결과를 동기식으로 기다립니다.
         LlmReportDto llmReportDto = llmService.generateReport(report).block();
 
         if (llmReportDto == null) {
@@ -94,10 +91,30 @@ public class WellnessReportServiceImpl implements WellnessReportService {
         report.setReportTitle(llmReportDto.getTitle());
         report.setReportContent(llmReportDto.getContent());
         report.setReportFeedback(llmReportDto.getFeedback());
-        wellnessReportRepository.save(report);
 
         return new GenerateReportRes(
                 report.getId(),
+                report.getReportTitle(),
+                report.getReportContent(),
+                report.getReportFeedback()
+        );
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public GetWellnessReportRes getReport(Long reportId,Long userId) {
+
+        WellnessReport report = wellnessReportRepository.findById(reportId)
+                .orElseThrow(() -> new CustomException(ReportErrorResponseCode.NOT_FOUND_REPORT));
+
+        if (!report.getUser().getId().equals(userId)) {
+            throw new CustomException(ReportErrorResponseCode.NO_PERMISSION);
+        }
+
+        return new GetWellnessReportRes(
+                report.getId(),
+                report.getUser().getId(),
+                report.getPass().getName(),
                 report.getReportTitle(),
                 report.getReportContent(),
                 report.getReportFeedback()
