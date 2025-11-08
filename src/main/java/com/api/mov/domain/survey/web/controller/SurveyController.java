130e4ec4e8cdf5a594f3c5f260bce7883b20e113
zzuhannn <lunaaeric41@naver.com>
2025-11-09 01:47:35 +0900

    refactor : 권한 인증 기능 추가

--- src/main/java/com/api/mov/domain/survey/web/controller/SurveyController.java
+++ src/main/java/com/api/mov/domain/survey/web/controller/SurveyController.java
@@ -3,11 +3,13 @@ package com.api.mov.domain.survey.web.controller;
 import com.api.mov.domain.survey.service.SurveyService;
 import com.api.mov.domain.survey.web.dto.CreateSurveyReq;
 import com.api.mov.domain.survey.web.dto.CreateSurveyRes;
+import com.api.mov.global.jwt.UserPrincipal;
 import com.api.mov.global.response.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,8 @@ public class SurveyController {
     private final SurveyService surveyService;
 
     @PostMapping
-    public ResponseEntity<SuccessResponse<?>> createSurvey(@Valid @RequestBody CreateSurveyReq createSurveyReq) {
-        CreateSurveyRes createSurvey = surveyService.createSurvey(createSurveyReq);
+    public ResponseEntity<SuccessResponse<?>> createSurvey(@Valid @RequestBody CreateSurveyReq createSurveyReq, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        CreateSurveyRes createSurvey = surveyService.createSurvey(createSurveyReq,userPrincipal.getId());
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
