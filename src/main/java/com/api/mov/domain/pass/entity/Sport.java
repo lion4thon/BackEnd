c60ffc693d07545d73da68400ef1737864cae501
naeunmin <babaleo115@gmail.com>
2025-11-08 15:17:55 +0900

    refactor : 연관관계 변경에 의해 삭제

--- src/main/java/com/api/mov/domain/pass/entity/Sport.java
+++ src/main/java/com/api/mov/domain/pass/entity/Sport.java
@@ -20,6 +20,4 @@ public class Sport {
 //    @Column(nullable = false)
 //    private String tags; //종목 특징? 느낌?
 
-    @OneToMany(mappedBy = "sport", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PassItem> passItems = new ArrayList<>();
 }
