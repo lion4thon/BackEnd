740b1ceb8d10ab1c54a089e2b4e3115e9777c619
zzuhannn <lunaaeric41@naver.com>
2025-11-09 05:39:04 +0900

    feat: 패키지 상세 조회 controller 추가

--- src/main/java/com/api/mov/domain/pass/web/controller/PassController.java
+++ src/main/java/com/api/mov/domain/pass/web/controller/PassController.java
@@ -2,6 +2,7 @@ package com.api.mov.domain.pass.web.controller;
 
 import com.api.mov.domain.pass.service.PassService;
 import com.api.mov.domain.pass.web.dto.PassCreateReq;
+import com.api.mov.domain.pass.web.dto.PassRes;
 import com.api.mov.global.jwt.UserPrincipal;
 import com.api.mov.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,10 +10,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +26,13 @@ public class PassController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(SuccessResponse.success("패키지 생성을 성공했습니다."));
+    }
+    // 패키지 상세조회 api
+    @GetMapping("/pass/{passId}")
+    public ResponseEntity<SuccessResponse<?>> getPassDetail(@PathVariable Long passId) {
+        PassRes passDetail = passService.getPassDetail(passId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessResponse.ok(passDetail));
     }
 }
