d6a7c18747b26b44fefb41b2a34d3e23b7d2336d
naeunmin <babaleo115@gmail.com>
2025-11-08 15:16:49 +0900

    refactor : 역직렬화를 위한 Setter 추가

--- src/main/java/com/api/mov/domain/pass/web/dto/PassCreateReq.java
+++ src/main/java/com/api/mov/domain/pass/web/dto/PassCreateReq.java
@@ -1,10 +1,12 @@
 package com.api.mov.domain.pass.web.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 public class PassCreateReq {
     private List<Long> facilityIdList;
     private int passPrice;
