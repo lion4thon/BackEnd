003d3684591490791416a2493cbfcbfe3db920d1
naeunmin <babaleo115@gmail.com>
2025-11-09 23:08:48 +0900

    feat : 결제 로직을 위한 조회 및 수정 메소드 추가 featch join으로 N+1 문제 없이 사용자 패스 정보를 조회하는 findAllByUserIdAndPassIdInWithPass 메소드 추가

--- src/main/java/com/api/mov/domain/pass/repository/UserPassRepository.java
+++ src/main/java/com/api/mov/domain/pass/repository/UserPassRepository.java
@@ -16,4 +16,7 @@ public interface UserPassRepository extends JpaRepository<UserPass, Long> {
     "join FETCH  f.sport " +
     "WHERE up.user.id = :userId")
     List<UserPass> findAllByUserIdWithDetails(@Param("userId") Long userId);
+
+    @Query("SELECT up FROM UserPass up JOIN FETCH up.pass WHERE  up.user.id = :userId and up.pass.id IN :passIds")
+    List<UserPass> findAllByUserIdAndPassIdInWithPass(@Param("userId")Long userId, @Param("passIds") List<Long> passIds);
 }
