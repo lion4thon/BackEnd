6278b001e1d45d78f78e53e404fdfa0e1291eb89
naeunmin <babaleo115@gmail.com>
2025-11-09 13:08:21 +0900

    feat : 내가 만든 패키지 조회 API 엔드포인트 작성

--- src/main/java/com/api/mov/domain/pass/web/controller/PassController.java
+++ src/main/java/com/api/mov/domain/pass/web/controller/PassController.java
@@ -1,6 +1,7 @@
 package com.api.mov.domain.pass.web.controller;
 
 import com.api.mov.domain.pass.service.PassService;
+import com.api.mov.domain.pass.web.dto.MyPassRes;
 import com.api.mov.domain.pass.web.dto.PassCreateReq;
 import com.api.mov.global.jwt.UserPrincipal;
 import com.api.mov.global.response.SuccessResponse;
@@ -9,10 +10,9 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +28,13 @@ public class PassController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(SuccessResponse.success("패키지 생성을 성공했습니다."));
+    }
+
+    @GetMapping("/my-passes")
+    public ResponseEntity<SuccessResponse<?>> getMyPasses(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        List<MyPassRes> myPassRes = passService.getMyPassList(userPrincipal.getId());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessResponse.ok(myPassRes));
     }
 }
