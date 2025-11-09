2f207a88f2b4b5e417d8241939de591341f51a70
naeunmin <babaleo115@gmail.com>
2025-11-09 13:09:40 +0900

    feat : 내가 만든 패키지 조회 API를 위한 JPQL 작성 fetch join

--- src/main/java/com/api/mov/domain/pass/repository/UserPassRepository.java
+++ src/main/java/com/api/mov/domain/pass/repository/UserPassRepository.java
@@ -2,6 +2,18 @@ package com.api.mov.domain.pass.repository;
 
 import com.api.mov.domain.pass.entity.UserPass;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserPassRepository extends JpaRepository<UserPass, Long> {
+
+    @Query("SELECT up FROM UserPass up " +
+    "JOIN FETCH up.pass p " +
+    "JOIN FETCH  p.passItems pi " +
+    "JOIN FETCH  pi.facility f " +
+    "join FETCH  f.sport " +
+    "WHERE up.user.id = :userId")
+    List<UserPass> findAllByUserIdWithDetails(@Param("userId") Long userId);
 }
