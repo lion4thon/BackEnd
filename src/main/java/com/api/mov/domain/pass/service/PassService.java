bb3877133840e3761b5d536aeac9ca9b191818ca
naeunmin <babaleo115@gmail.com>
2025-11-09 13:08:38 +0900

    feat : 내가 만든 패키지 조회 API 서비스 인터페이스 작성 및 구현

--- src/main/java/com/api/mov/domain/pass/service/PassService.java
+++ src/main/java/com/api/mov/domain/pass/service/PassService.java
@@ -1,10 +1,12 @@
 package com.api.mov.domain.pass.service;
 
 import com.api.mov.domain.facility.entity.Facility;
+import com.api.mov.domain.pass.web.dto.MyPassRes;
 import com.api.mov.domain.pass.web.dto.PassCreateReq;
 
 import java.util.List;
 
 public interface PassService {
     void createPass(PassCreateReq passCreateReq, Long userId);
+    List<MyPassRes> getMyPassList(Long userId);
 }
