5e84846d2cf1bba44d4ac7e0494312f523cfa787
zzuhannn <lunaaeric41@naver.com>
2025-11-10 15:31:07 +0900

    feat: 패키지 보관함 상태값 추가

--- src/main/java/com/api/mov/domain/pass/entity/UserPassStatus.java
+++ src/main/java/com/api/mov/domain/pass/entity/UserPassStatus.java
@@ -2,6 +2,7 @@ package com.api.mov.domain.pass.entity;
 
 public enum UserPassStatus {
     IN_CART,    // 패키지 보관함
+    IN_LOCKER,  // 패키지 보관함
     OWNED,      // 진행 중인 패키지
     COMPLETED   // 완료된 패키지
 }
